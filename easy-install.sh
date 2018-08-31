d477b4b8079507e0c1a2f3c9a15cb5d4b2f2be09
WestleyK <westley@sylabs.io>
2018-08-31 23:28:25 +0000

    added easy-install.sh

---
+++ easy-install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Created by: Westley K
+# email: westley@sylabs.io
+# Date: Aug 31, 2018
+# version-1.0.0
+# https://github.com/WestleyK/rpi-brightness
+#
+# MIT License
+#
+
+set -e
+
+SCRIPT_URL=""
+SCRIPT_NAME="rpi-brightness"
+INSTALL_PATH="/usr/local/bin/"
+ARCH_VERSION="armv7l"
+
+echo "Install script version: version-1.0.0"
+echo 
+
+ARCH_ON=` uname -m `
+if [ "$ARCH_ON" != "$ARCH_VERSION" ]; then
+    printf "\033[0;31mFATAL ERROR: \033[0m"
+    echo "Architecture not supported!"
+    echo "Supported architecture version: $ARCH_VERSION"
+    echo "Your architecture version: $ARCH_ON"
+    echo 
+    echo "FAIL: Install failed!"
+    exit 1
+fi
+
+echo "Downloading file..."
+wget -O $SCRIPT_NAME $SCRIPT_URL
+chmod +x $SCRIPT_NAME
+
+echo 
+echo
+echo
+echo "SUCCESS: install successful."
+echo 
+echo "########## $SCRIPT_NAME installed to the current directory  ##########"
+echo
+echo ">> To finish the install, do:"
+echo " $ sudo mv $SCRIPT_NAME /usr/local/bin/"
+exit 0
+
+#
+# End install script
+#
